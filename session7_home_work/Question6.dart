86796dfa12a5170eb523d018266e31084dfc54db
jafarhussam <jafarking345@gmail.com>
2025-09-07 01:19:18 +0300

    session7_home_work

--- session7_home_work/Question6.dart
+++ session7_home_work/Question6.dart
@@ -1,6 +1,25 @@
 //Q6. Number Guessing (3 Tries) - Generate a random number between 1 and 20. - Let the user
 //guess up to 3 times. If they fail, reveal the correct number.
 
-void main(){
-  
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  int secretNumber = Random().nextInt(20) + 1;
+
+  print('enter your guess');
+  for (int attempt = 0; attempt <= 3; attempt++) {
+    int guess = int.parse(stdin.readLineSync()!);
+
+    if (guess < 1 || guess > 20) {
+      print('Please enter a valid number between 1 and 20');
+      attempt--;
+    }
+    if (guess == secretNumber) {
+      print('Congratulation! you guessed the number');
+      return;
+    } else{
+      print("Sorry, you didn't guess it. The correct number was $secretNumber.");
+    }
+  } 
 }
