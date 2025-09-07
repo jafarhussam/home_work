bff6c9156cc6510b6b339bf4b3d419af44b7a800
jafarhussam <jafarking345@gmail.com>
2025-09-07 13:05:24 +0300

    session7_home_work

--- session7_home_work/leetcode_valid_palindrome.dart
+++ session7_home_work/leetcode_valid_palindrome.dart
@@ -1,17 +1,6 @@
   class Solution {
-  bool? isPalindrome(String s) {
-    s = s.toLowerCase().replaceAll(RegExp('[^a-z0-9]'), '');
-
-    int left = 0;
-    int right = s.length - 1;
-
-    while (left < right) {
-      if (s[left] != s[right]) {
-        return false;
-      }
-      left++;
-      right--;
-    }
-    return true;
+  bool isPalindrome(String s) {
+ String cleaned = s.replaceAll(RegExp(r'[^a-zA-Z0-9]'), '').toLowerCase();
+     return cleaned == cleaned.split("").reversed.join("");
   }
 }
